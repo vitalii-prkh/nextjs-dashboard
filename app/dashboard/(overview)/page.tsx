d7a6c7c1e4a98d4d6135bd7f15879be046e77210
Vitaliy Parkhomenko <parkhomenko.wd@gmail.com>
2025-08-16 15:46:01 +0300

    Chapter 9 - Streaming

---
+++ app/dashboard/(overview)/page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { lusitana } from '@/app/ui/fonts';
+import CardWrapper from '@/app/ui/dashboard/cards';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
+
+export default async function Page() {
+  return (
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+        <React.Suspense fallback={<CardsSkeleton />}>
+        <CardWrapper />
+        </React.Suspense>
+      </div>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <React.Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </React.Suspense>
+        <React.Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </React.Suspense>
+      </div>
+    </main>
+  );
+}
