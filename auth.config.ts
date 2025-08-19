ace497b771a4dcd96004e374a6edc88699c841d6
Vitalii Parkhomenko <vitalii.prkh+github@gmail.com>
2025-08-19 08:42:29 +0300

    Chapter 15 - Add authentication flow with sign-in, error handling, and session management (#7)

---
+++ auth.config.ts
@@ -0,0 +1,23 @@
+import type {NextAuthConfig} from 'next-auth';
+
+export const authConfig = {
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: {nextUrl} }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+
+      if (isOnDashboard) {
+        // if `!isLoggedIn` - redirect unauthenticated users to the login page
+        return isLoggedIn;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+
+      return true;
+    },
+  },
+  providers: [], // Add providers with an empty array for now
+} satisfies NextAuthConfig;
